2d8f3dbb2f074f5ef792db7022855aeb7d75ec0f
Yury Selivanov <yselivanov@sprymix.com>
2008-10-20 13:30:15 -0400

    config: Refactor configuration to use the new import machinery

--- importkit/__init__.py
+++ importkit/__init__.py
@@ -1,11 +1,11 @@
-from semantix.lib.readers.yaml import YamlReader
+from semantix.lib.readers.yaml.reader import YamlReader
 
 __all__ = ['read']
 
 class SemantixReaderError(Exception): pass
 
-def read(filename):
+def read(filename, return_meta = False):
     if filename.endswith('.yml'):
-        return YamlReader.read(filename)
+        return YamlReader.read(filename, return_meta)
 
     raise SemantixReaderError('cannot read file:  %s' % filename)
---
+++ importkit/yaml/schema.py
@@ -0,0 +1,21 @@
+import subprocess
+
+class YamlValidationError(Exception): pass
+class Base(object):
+    schema_file = ''
+
+    @classmethod
+    def validate(cls, filename):
+        kwalify = subprocess.Popen(['kwalify', '-lf', cls.schema_file, filename],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        (stdout, stderr) = kwalify.communicate()
+
+        if stdout.find('INVALID') >= 0 or stderr.find('ERROR') >= 0:
+            raise YamlValidationError('Failed to validate file: %s\n\nValidator output: \n%s' %
+                                           (filename, stderr + stdout))
+
+    @classmethod
+    def _create_class(cls, meta, dct):
+        cls.schema_file = meta['filename']
+        return type(meta['class']['name'], (Base,), {'schema_file': meta['filename']})
