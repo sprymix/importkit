397a8d72f273bc05a459faa22551c454fdd931f9
Elvis Pranskevichus <elprans@sprymix.com>
2015-11-03 10:56:21 -0500

    importkit-0.5.3

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def find_packages(*roots):
 
 setup_args = {
     'name':             'importkit',
-    'version':          '0.5.2',
+    'version':          '0.5.3',
     'description':      ('Importkit is a Python library for making anything '
                          'importable as a Python module.'),
     'long_description': readme,
